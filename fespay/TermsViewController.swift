8bd13c9ffa22a3e8f5b0e1dfd8735b16c96bad8c
xiz-tky <shizuka.kakimoto@linkage.jp.net>
2017-09-14 13:43:54 +0900

    load local terms_of_use_tenant.html

--- fespay/TermsViewController.swift
+++ fespay/TermsViewController.swift
@@ -7,8 +7,9 @@
 //
 
 import UIKit
+import os.log
 
-class TermsViewController: UIViewController {
+class TermsViewController: UIViewController, UIWebViewDelegate {
 
     let i18n = I18n(tableName: "TermsView")
     
@@ -31,6 +32,10 @@ class TermsViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        webView.delegate = self
+        
+        LoadingProxy.set(self)
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -39,7 +44,8 @@ class TermsViewController: UIViewController {
     }
     
     override func viewDidAppear(_ animated: Bool) {
-        let pathToTerms = URL(string: "https://s3-ap-northeast-1.amazonaws.com/fespay-prd/legal/terms_of_use_tenant.html")
+        // let pathToTerms = URL(string: "https://s3-ap-northeast-1.amazonaws.com/fespay-prd/legal/terms_of_use_tenant.html")
+        let pathToTerms = URL(string: Bundle.main.path(forResource: "terms_of_use_tenant", ofType: "html")!)
         let urlRequest = URLRequest(url: pathToTerms!)
         webView.loadRequest(urlRequest as URLRequest)
         
@@ -55,6 +61,24 @@ class TermsViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    func webViewDidStartLoad(_ webView: UIWebView) {
+        LoadingProxy.on()
+    }
+    
+    func webViewDidFinishLoad(_ webView: UIWebView) {
+        LoadingProxy.off()
+    }
+    
+    func webView(_ webView: UIWebView, didFailLoadWithError error: Error) {
+        os_log("webView Load Error: %@", log: .default, type: .error, error as CVarArg)
+        LoadingProxy.off()
+        
+        let alert = self.i18n.alert(titleKey: "loadFailed", messageKey: "tryAgain", handler: {
+            self.dismiss(animated: true, completion: nil)
+        })
+        self.present(alert, animated: true)
+    }
+    
     /*
     // MARK: - Navigation
 
